19683ceb8f6f5ad0e9b4b15197a74522f67c7dc5
António Amaral <antonio.amaral.7778@gmail.com>
2024-01-28 20:40:20 -0100

    Implemented Hamburger Menu Functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,30 @@
 import "./Header.scss";
 import hamburgerMenu from "../assets/Hamburger Menu.svg";
 import PrimaryButton from "./PrimaryButton";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+  };
+
   return (
     <header>
-      <span className="logo">Chirp.</span>
-
-      <button type="button" className="hamburger-menu-button">
-        <img src={hamburgerMenu} alt="Hamburger Menu" />
-      </button>
+      <div className="mobile-header-wrapper">
+        <span className="logo">Chirp.</span>
+        <button
+          type="button"
+          className="hamburger-menu-button"
+          onClick={toggleMenu}
+        >
+          <img src={hamburgerMenu} alt="Hamburger Menu" />
+        </button>
+      </div>
 
       <div className="navigation-wrapper">
-        <nav>
+        <nav className={isMenuOpen ? "" : "navigation-closed"}>
           <ul className="navigation-list">
             <li>
               <a href="#">Home</a>
@@ -26,7 +38,9 @@ const Header = () => {
           </ul>
         </nav>
 
-        <PrimaryButton text="Sign in with Twitter" />
+        <div className="sign-in-button">
+          <PrimaryButton text="Sign in with Twitter" />
+        </div>
       </div>
     </header>
   );
